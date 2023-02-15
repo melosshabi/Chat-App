d7030e7e43443670a1faf7622aacf3c1950bb54b
melosshabi <melosshabi05@gmail.com>
2023-02-15 19:54:51 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ function App() {
   return (
     <Router>
     <Routes>
-      <Route path="/chatApp/signIn" element={<SignIn/>}/>
-      <Route path="/chatApp/signUp" element={<SignUp/>}/>
-      <Route path="/chatApp" element={<Home/>}/>
-      <Route path="/chatApp/userProfile" element={<UserProfile/>}/>
+      <Route path="/chatApp" exact element={<Home/>}/>
+      <Route path="/chatApp/signIn" exact element={<SignIn/>}/>
+      <Route path="/chatApp/signUp" exact element={<SignUp/>}/>
+      <Route path="/chatApp/userProfile" exact element={<UserProfile/>}/>
     </Routes>
     </Router>
   );
